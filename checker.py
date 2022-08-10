539a92245639b390b2b5950e2d13432770ac66bc
Alexander R. Tomsovic <84757117+alexandertomsovic@users.noreply.github.com>
2022-08-09 21:40:13 -0700

    checker.py

--- checker.py
+++ checker.py
@@ -1,3 +1,5 @@
+# Crypto price checker bot by Alexander Tomsovic
+
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
@@ -10,10 +12,10 @@ def clearpage():
   
 # Webscraping function for Bitcoin
 def btc_prices():
-  btc_url='https://www.coingecko.com/' # URL we use
+  btc_url='https://crypto.com/price/bitcoin' # URL we use
   btc_response = requests.get(btc_url)
   btc_soup = BeautifulSoup(btc_response.text, 'html.parser') # soup variable parses 
-  btc_price = btc_soup.find('span', {"class": "no-wrap"}).text # Bitcoin price
+  btc_price = btc_soup.find('span', {"class": "chakra-text css-13hqrwd"}).text # Bitcoin price
   return btc_price
 
 # Webscraping function for Etherium
@@ -26,43 +28,48 @@ def eth_prices():
 
 btc_price = btc_prices()
 eth_price = eth_prices()
-print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + str(btc_price) + " USD")
-print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + str(eth_price) + "\n")
-
-# Bitcoin price checker
-
-
+print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + str(btc_price))
+print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + str(eth_price))
 
 # Etherium price checker
 
 checker = 1
 
 while True:
+  print("")
   print(Fore.MAGENTA + str(checker) + Fore.WHITE + " ticks since last price change")
   checker += 1
-  sleep(0.1)
+  sleep(0.001)
   clearpage()
   
   new_btc_prices = btc_prices()
   new_eth_prices = eth_prices()
   
-  if new_eth_prices != eth_prices():
-    if new_btc_prices != btc_prices():
+  if new_eth_prices != eth_prices or new_btc_prices != btc_prices():
+    
+    checker = 1
+    
+    btc_price = btc_prices()
+    eth_price = eth_prices()
+
+    clearpage()
+    clearpage()
+    clearpage()
+
+    if btc_price > new_btc_prices: # Bitcoin up / down movement color coder
+      print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(btc_price))
+    elif btc_price < new_btc_prices:
+      print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.RED + str(btc_price))
+    else:
+        print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.WHITE + str(btc_price))
       
-      eth_price = eth_prices()
-      btc_price = btc_prices()
 
-      if btc_price > new_btc_prices: # Bitcoin up / down movement color coder
-        print(Fore.LIGHTYELLOW_EX + "\nBitcoin " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(btc_price) + " USD")
-      elif btc_price < new_btc_prices:
-        print(Fore.LIGHTYELLOW_EX + "\nBitcoin " + Fore.WHITE + "is trading at " + Fore.RED + str(btc_price) + " USD")
-      else:
-        print(Fore.LIGHTYELLOW_EX + "\nBitcoin " + Fore.WHITE + "is trading at " + Fore.WHITE + str(btc_price) + " USD")
+    if eth_price > new_eth_prices:
+      print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(eth_price))
+    elif eth_price < new_eth_prices:
+      print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.RED + str(eth_price))
+    else:
+      print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.WHITE + str(eth_price))
 
-      if eth_price > new_eth_prices:
-        print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(eth_price))
-      elif eth_price < new_eth_prices:
-        print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.RED + str(eth_price))
-      else:
-        print(Fore.LIGHTCYAN_EX + "Etherium " + Fore.WHITE + "is trading at " + Fore.RED + str(eth_price))
-      checker = 1
+    
+  sleep(1)
