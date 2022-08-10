0ae3e2a2678512473de6a85f91f1e1b9fd7eea1c
Alexander R. Tomsovic <84757117+alexandertomsovic@users.noreply.github.com>
2022-08-09 23:01:20 -0700

    checker.py

--- checker.py
+++ checker.py
@@ -26,16 +26,30 @@ def eth_prices():
   eth_price = eth_soup.find('span', {"class": "chakra-text css-13hqrwd"}).text # Ethereum price
   return eth_price
 
+# Webscraping function for XRP
+def xrp_prices():
+  xrp_url = 'https://crypto.com/price/xrp'
+  xrp_response = requests.get(xrp_url)
+  xrp_soup = BeautifulSoup(xrp_response.text, 'html.parser')
+  xrp_price = xrp_soup.find('span', {"class": "chakra-text css-13hqrwd"}).text # Ethereum price
+  return xrp_price
+
+# Attributes function price to actual price
 btc_price = btc_prices()
 eth_price = eth_prices()
+xrp_price = xrp_prices()
+
+# Prints prices in terminal
 print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + str(btc_price))
 print(Fore.LIGHTCYAN_EX + "Ethereum " + Fore.WHITE + "is trading at " + str(eth_price))
+print(Fore.LIGHTBLACK_EX + "XRP " + Fore.WHITE + "is trading at " + str(xrp_price))
 
-# Ethereum price checker
+# price checker
 
 checker = 1
 
 while True:
+
   print("")
   print(Fore.MAGENTA + str(checker) + Fore.WHITE + " ticks since last price change")
   checker += 1
@@ -44,32 +58,39 @@ while True:
   
   new_btc_prices = btc_prices()
   new_eth_prices = eth_prices()
-  
+  new_xrp_prices = xrp_prices()
+
   if new_eth_prices != eth_prices or new_btc_prices != btc_prices():
-    
-    checker = 1
     
     btc_price = btc_prices()
     eth_price = eth_prices()
-
+    xrp_price = xrp_prices()
+    
     clearpage()
     clearpage()
     clearpage()
-
+    clearpage()
     if btc_price > new_btc_prices: # Bitcoin up / down movement color coder
       print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(btc_price))
     elif btc_price < new_btc_prices:
       print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.RED + str(btc_price))
     else:
-        print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.WHITE + str(btc_price))
-      
+        print(Fore.LIGHTYELLOW_EX + "Bitcoin " + Fore.WHITE + "is trading at " + Fore.WHITE + str(btc_price)) 
 
-    if eth_price > new_eth_prices:
+    if eth_price > new_eth_prices: # Ethereum up / down movement color coder
       print(Fore.LIGHTCYAN_EX + "Ethereum " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(eth_price))
     elif eth_price < new_eth_prices:
       print(Fore.LIGHTCYAN_EX + "Ethereum " + Fore.WHITE + "is trading at " + Fore.RED + str(eth_price))
     else:
       print(Fore.LIGHTCYAN_EX + "Ethereum " + Fore.WHITE + "is trading at " + Fore.WHITE + str(eth_price))
 
-    
+    if xrp_price > new_xrp_prices: # XRP up / down movement color coder
+      print(Fore.LIGHTBLACK_EX + "XRP " + Fore.WHITE + "is trading at " + Fore.LIGHTGREEN_EX + str(xrp_price))
+    elif xrp_price < new_xrp_prices:
+      print(Fore.LIGHTBLACK_EX + "XRP " + Fore.WHITE + "is trading at " + Fore.RED + str(xrp_price))
+    else:
+      print(Fore.LIGHTBLACK_EX + "XRP " + Fore.WHITE + "is trading at " + Fore.WHITE + str(xrp_price))
+
+    checker = 1
   sleep(1)
+  
